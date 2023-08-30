aa80386f5c77fbcd8b229298e3139fff98fbc6d2
irsen <irsen.kan@bk.ru>
2023-08-30 12:09:41 +0500

    Initial commit

---
+++ app/src/main/java/kan/kis/learnAndroidApp/Presentation/MenuActivity.kt
@@ -0,0 +1,15 @@
+package kan.kis.learnAndroidApp.Presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kan.kis.learnAndroidApp.R
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
